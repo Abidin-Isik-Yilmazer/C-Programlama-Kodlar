9f22e8404481c272f9e903be310f8bc092013fb0
Abidin Işık Yılmazer <abidinylmzer58@gmail.com>
2026-02-19 17:09:18 +0300

    Add files via upload

---
+++ sayinin_tek_veya_cift_oldugunu_bulan_program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+	int sayi;
+	printf("Sayiyi giriniz= ");
+	scanf("%d",&sayi);
+	if(sayi%2==0)
+	 printf("sayi cifttir.");
+	else
+	 printf("sayi tektir");
+	return 0;
+}
