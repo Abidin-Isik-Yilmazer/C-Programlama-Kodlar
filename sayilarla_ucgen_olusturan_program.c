7d28ecda932d153c41d30365fe02c315ab14bea1
Abidin Işık Yılmazer <abidinylmzer58@gmail.com>
2026-02-19 19:10:22 +0300

    Add files via upload

---
+++ sayilarla_ucgen_olusturan_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void){
+	int i,j,k;
+	for(i=1;i<=7;i++){
+		for(k=1;k<=7-i;k++)
+		 printf(" ");
+		for(j=i;j>=1;j--)
+		 printf("%d ",j);
+	printf("\n");
+	}
+	return 0;
+}
